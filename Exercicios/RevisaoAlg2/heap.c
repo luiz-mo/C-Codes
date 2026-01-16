1fad53415dad0f00d7313c75b87d2f6debd222a5
Luiz Mo <luizming07@gmail.com>
2026-01-16 19:09:28 -0300

    [add]heap

---
+++ Exercicios/RevisaoAlg2/heap.c
@@ -0,0 +1,25 @@
+void heapfy(int v[], int n){
+
+}
+
+void insereHeap(int v[], int n){
+    
+}
+
+void removeHeap(int v[], int n){
+
+}
+
+int checaHeap(int v[], int n){
+    int i;
+
+    for(i=n;i > 0;i--){
+        if(v[i] > v[i/2])
+            return 0;
+        i /= 2;
+    }
+}
+
+void sacodeHeap(int v[], int n){
+
+}
---
+++ Exercicios/RevisaoAlg2/heap.h
@@ -0,0 +1,9 @@
+void heapfy(int v[], int n);
+
+void insereHeap(int v[], int n);
+
+void removeHeap(int v[], int n);
+
+int checaHeap(int v[], int n);
+
+void sacodeHeap(int v[], int n);
