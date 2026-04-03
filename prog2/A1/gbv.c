a4050932fcc16a3134ecf054c96a8135a478534f
Luiz Mo <luizming07@gmail.com>
2026-04-02 22:11:17 -0300

    inicio do gbv_add

--- prog2/A1/gbv.c
+++ prog2/A1/gbv.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
 #include "gbv.h"
@@ -59,8 +60,48 @@ int gbv_open(Library *lib, const char *filename){
 }
 
 int gbv_add(Library *lib, const char *archive, const char *docname){
+    size_t data_read;
+    struct superBloco sb;
+    FILE *gbv, *doc;
+    char buffer[BUFFER_SIZE];
+    Document new_doc;
+    long doc_size = 0;
 
+    /*abre a biblioteca*/
+    if(!(gbv = fopen(archive,"rb+")))
+        return 1;
 
+    /*cria o documento a ser inserido*/
+    if(!(doc = fopen(docname, "rb"))){
+        fclose(gbv);
+
+        return 1;
+    }
+
+    /*le superbloco*/
+    if(fread(&sb,sizeof(struct superBloco),1,gbv) != 1){
+        fclose(gbv);
+        fclose(doc);
+
+        return 1;
+    }
+
+    /*procura a posicao de escrever o novo documento*/
+    fseek(gbv,sb.offset,SEEK_SET);
+    
+    /*itera enquanto ainda houverem bytes nao lidos*/
+    while((data_read = fread(buffer,1,BUFFER_SIZE,doc) > 0)){
+        fwrite(buffer,1,data_read,gbv); /*escreve na biblioteca o que esta no buffer*/
+        doc_size += data_read;
+    }
+
+    strcpy(new_doc.name,docname); 
+    new_doc.offset = ftell(gbv);
+    new_doc.size = doc_size;
+
+    lib->docs[lib->count] = new_doc;
+    
+    sb.offset
 }
 
 int gbv_remove(Library *lib, const char *docname){
@@ -75,9 +116,16 @@ int gbv_list(const Library *lib){
         return 0;
     }
 
-    for
+    for(i=0; i<lib->count; i++){
+        Document d = lib->docs[i];
 
-    
+        printf("Nome: %s\n", d.name);
+        printf("Tamanho: %ld bytes\n", d.size);
+        printf("Data de inserção: %s", d.date);
+        printf("Offest: %d", d.offset);
+    }
+
+    return 0;
 }
 
 int gbv_view(const Library *lib, const char *docname){
@@ -85,5 +133,16 @@ int gbv_view(const Library *lib, const char *docname){
 }
 
 int gbv_order(Library *lib, const char *archive, const char *criteria){
+    if(strcmp(criteria, "nome"))
+
+    else if(strcmp(criteria, "data"))
+
+    else if(strcmp(criteria, "tamanho"))
+
+    else{
+        printf("Criterio invalido");
+        return 1;
+    }
+
 
 }
